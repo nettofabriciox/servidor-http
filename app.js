856b404b94b0c1a2d0994a6c9c532dc323ca9e22
fabricionetto <agostinhonetto73@gmail.com>
2024-07-30 10:45:37 -0300

    Recebendo dados requeridos

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const http = require('http');
 const porta = 3000
+const url = require('url')
 
 const servidor = http.createServer((requisicao, resposta)=>{
-    resposta.writeHead(200, {'Content-Type':'text/plain'});
-    resposta.end('Ola, sou servidor');
+    resposta.writeHead(200, {'Content-Type':'text/plain'})
+    var dadosUrl = url.parse(requisicao.url, true).query;
+    var texto = dadosUrl.ano + " " +dadosUrl.mes;
+    resposta.end(texto);
 })
 
 servidor.listen(porta,()=>{
